4d6ee85833bc8004a788081bfd7ca828240f4576
JeonHanSem <rmsdlf159@naver.com>
2016-05-24 23:14:37 +0900

    button

---
+++ test_Resource/buttonclick.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class buttonclick : MonoBehaviour {
+    public InputField mainInputField;
+    public GameObject login;
+    public GameObject record;
+    public GameObject input;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    public void clickLogin()
+    {
+        parameter.id = mainInputField.text;
+        parameter.loginFlag = 1;
+        Debug.Log(parameter.id);
+        login.SetActive(false);
+        input.SetActive(false);
+        record.SetActive(true);
+
+    }
+    public void clickRecord()
+    {
+        parameter.dbView = true;
+        parameter.lFlag = true;
+    }
+    public void clickLogout()
+    {
+        parameter.loginFlag = 0;
+        parameter.startFlag = true;
+    }
+    public void clickExit()
+    {
+        Debug.Log("h");
+        parameter.startFlag = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
